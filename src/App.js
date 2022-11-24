22e1c0232f2c1cd28242a0413baa8a05281f0aed
Femi Bandele <105954155+bandelefemi@users.noreply.github.com>
2022-11-24 17:59:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Switch, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import {Navbar} from './components'
+import './App.css'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='navBar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+        main
+      </div>
+      <div className='footer'>
+        footer
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd'
+import { HomeOutlined, BulbOutlined, FundOutlined, MenuOutlined, MoneyCollectOutlined, } from '@ant-design/icons'
+import {Link} from 'react-router-dom'
+import icon from '../images/cryptocurrency.png'
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+      <div className='logo-container'>
+        <Avatar src={icon} size='large' />
+        <Typography.Title level={2} className='logo'>
+            <Link to={'/'}>
+                CryptoApp
+            </Link>
+        </Typography.Title>
+        <Button className='menu-control-container'>
+            
+        </Button>
+      </div>
+      <Menu theme='dark' >
+        <Menu.Item icon={<HomeOutlined />} >
+            <Link to={'/'} >Home</Link>
+        </Menu.Item>
+      </Menu>
+    </div>
+  )
+}
+
+export default Navbar
